0e3a333e1ee5e73dea910fd225962c44ae36b59d
nakada toyohisa <nakada.toyohisa@gmail.com>
2026-06-12 14:26:48 +0900

    lang=ja

--- src/problems/index00.ts
+++ src/problems/index00.ts
@@ -1,9 +1,11 @@
 import type { Problem } from './types';
 import problem001 from './problem001';
 import problem002 from './problem002';
+import problem067 from './problem067';
 
 // セット1（超入門：変数・演算・print）
 export const problemSet00: Problem[] = [
+  problem067,
   problem001,
   problem002,
 ];
