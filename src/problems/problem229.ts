872692da1837229fb22f185b77de2db353234a23
nakada toyohisa <nakada.toyohisa@gmail.com>
2026-07-10 09:47:12 +0900

    ヒントチョット調整

--- src/problems/problem229.ts
+++ src/problems/problem229.ts
@@ -2,7 +2,7 @@ import type { Problem } from './types';
 
 const problem: Problem = {
   id: 229,
-  title: "文字の最初に「Hello,」最後に「!」を付加する関数を作成してください",
+  title: "文字の最初に「Hello,」最後に「!」を付加する関数を作成してください。関数の引数(関数の実行パラメータ)をsとしたとき、return f\"{s}!\"とするとsに入っている文字に ! を付けた文字を返します",
   type: 'fill-in',
   codeWithMarker: `# defから書き始めます
 §§§§§§§§§§§§§§§§§§§§§§§§§§§§§
